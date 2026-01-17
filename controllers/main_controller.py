8c72502f488d088ac18f694d88e3be370ee5a256
Rai Kekely <raissak776@gmail.com>
2026-01-17 19:44:08 -0300

    Em controllers eu arrumei as rotas, tirando o login required e a antiga pagina de index agora é jogos.html e o index sera a pagina inicial com o template novo. Em static eu criei um novo arquivo em css para ser o css do index. Criei uma nova pasta com para as imagens para o index. Mudei a antiga pasta images para images-jogomemoria para facilitar a nnavegacao pelas coisas. Criei uma nova pasta para guardar os arquivos js e neles mudei o que era necessario, tanto para o funcionamento, quanto para que as imagens continuem aparecendo corretamente. Em templates eu criei um novo index como foi citado anteriormente, criei o jogos.html que era a antiga telaque aparecia quando se fazia login, nos atuais jogos mudei a parte do script, colocando que agora os arquivos estão dentro da pagina js, dentro de static

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -11,40 +11,25 @@ def init_app(app):
     app.add_url_rule('/', endpoint='index', view_func=index, methods=['GET'])
 
     # Jogos list (placeholder)
-    @login_required
+    
     def jogos_index():
         return render_template('jogos.html')
 
     app.add_url_rule('/jogos', endpoint='jogos_index', view_func=jogos_index, methods=['GET'])
 
-    @login_required
+    
     def jogo_memoria():
         return render_template('jogo_memoria.html')
 
     app.add_url_rule('/jogo-memoria', endpoint='jogo_memoria', view_func=jogo_memoria, methods=['GET'])
 
-    @login_required
+    
     def jogo_divisao():
         return render_template('jogo_divisao.html')
 
     app.add_url_rule('/jogo-divisao', endpoint='jogo_divisao', view_func=jogo_divisao, methods=['GET'])
 
-    def sobre():
-        return render_template('sobre.html')
-
-    app.add_url_rule('/sobre', endpoint='sobre', view_func=sobre, methods=['GET'])
-
-    def ajuda():
-        return render_template('ajuda.html')
-
-    app.add_url_rule('/ajuda', endpoint='ajuda', view_func=ajuda, methods=['GET'])
-
-    def politica_privacidade():
-        return render_template('politica_privacidade.html')
-
-    app.add_url_rule('/politica-privacidade', endpoint='politica_privacidade', view_func=politica_privacidade, methods=['GET'])
-
-
+   
     def jogo_multiplicacao():
         return render_template('jogo_multiplicacao.html')
 
