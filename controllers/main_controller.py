fb16ce6da537d261a67cc8ee7c5f155007fb2315
raiwyzs <raissak776@gmail.com>
2026-03-30 15:32:54 -0300

    Exclusão das coisas desnecessárias e correção dos nomes dos arquivos

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -29,37 +29,32 @@ def init_app(app):
         return render_template('jogos.html')
     app.add_url_rule('/jogos', endpoint='jogos_index', view_func=jogos_index, methods=['GET'])
 
-    
     def jogo_memoria():
-        return render_template('jogo_memoria.html')
+        return render_template('jogo-memoria.html')
     app.add_url_rule('/jogo-memoria', endpoint='jogo_memoria', view_func=jogo_memoria, methods=['GET'])
 
-    
     def jogo_divisao():
-        return render_template('jogo_divisao.html')
+        return render_template('jogo-divisao.html')
     app.add_url_rule('/jogo-divisao', endpoint='jogo_divisao', view_func=jogo_divisao, methods=['GET'])
 
-    def quiz_contas():
-        return render_template('quiz_contas.html')
-    app.add_url_rule('/quiz-contas', endpoint='quiz_contas', view_func=quiz_contas, methods=['GET'])
-    
-    def quiz_contexto():
-        return render_template('quiz_contexto.html')
-    app.add_url_rule('/quiz-contexto', endpoint='quiz_contexto', view_func=quiz_contexto, methods=['GET'])
-
-   
-
-
     def jogo_multiplicacao():
-        return render_template('jogo_multiplicacao.html')
+        return render_template('jogo-multiplicacao.html')
     app.add_url_rule('/jogo_multiplicacao', endpoint='jogo_multiplicacao', view_func=jogo_multiplicacao, methods=['GET'])
-
     
     def jogo_adicao():
-        return render_template('jogo_adicao.html')
+        return render_template('jogo-adicao.html')
     app.add_url_rule('/jogo-adicao', endpoint='jogo_adicao', view_func=jogo_adicao, methods=['GET'])
 
     def jogo_subtracao():
-        return render_template('jogo_subtracao.html')
+        return render_template('jogo-subtracao.html')
     app.add_url_rule('/jogo-subtracao', endpoint='jogo_subtracao', view_func=jogo_subtracao, methods=['GET'])
     
+
+    def quiz_contas():
+        return render_template('quiz-contas.html')
+    app.add_url_rule('/quiz-contas', endpoint='quiz_contas', view_func=quiz_contas, methods=['GET'])
+    
+    def quiz_contexto():
+        return render_template('quiz-contexto.html')
+    app.add_url_rule('/quiz-contexto', endpoint='quiz_contexto', view_func=quiz_contexto, methods=['GET'])
+
